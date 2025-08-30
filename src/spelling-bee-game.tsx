214ec613bed6d9dc326c63da3486c2d143d3a8a6
SquidgyG <david-gill@live.com>
2025-08-30 11:25:28 -0700

    chore: reference results screen typescript

--- src/spelling-bee-game.tsx
+++ src/spelling-bee-game.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import LeaderboardScreen from './LeaderboardScreen';
 import SetupScreen from './SetupScreen';
 import GameScreen from './GameScreen';
-import ResultsScreen from './ResultsScreen';
+import ResultsScreen from './ResultsScreen.tsx';
 import AchievementsScreen from './AchievementsScreen';
 import ShopScreen from '../ShopScreen';
 import useMusic from './utils/useMusic';
