feda1b6d5e18852ee7b632f2c9b241ee4f1b11e0
David Gill <david-gill@live.com>
2025-10-08 10:23:38 +1100

    Add AUDIO_ASSETS for sound effects and music

---
+++ assets/index.ts
@@ -0,0 +1,12 @@
+export const AUDIO_ASSETS = {
+  sfx: {
+    correct: '/sounds/correct.mp3',
+    wrong: '/sounds/wrong.mp3',
+    letterCorrect: '/sounds/letter-correct.mp3',
+    letterWrong: '/sounds/letter-wrong.mp3'
+  },
+  music: {
+    background: '/music/background_music.mp3',
+    menu: '/music/menu_music.mp3'
+  }
+};
